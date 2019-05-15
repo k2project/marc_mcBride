8a016da9ce46fd7311435a9e849018e960362a0e
k2project <kopczkrzy@gmail.com>
2019-05-15 23:56:10 +0100

    Updates

---
+++ precache-manifest.bce05002478591030f432c0506e49660.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e635ca07a8e52bcb4f96",
+    "url": "/marc_mcBride/static/js/runtime~main.fc873a1f.js"
+  },
+  {
+    "revision": "ac94842377b485ae7874",
+    "url": "/marc_mcBride/static/js/main.62147ce8.chunk.js"
+  },
+  {
+    "revision": "4231adcdc479f41c33cb",
+    "url": "/marc_mcBride/static/js/2.4947ba8c.chunk.js"
+  },
+  {
+    "revision": "ac94842377b485ae7874",
+    "url": "/marc_mcBride/static/css/main.3f822804.chunk.css"
+  },
+  {
+    "revision": "aec8bcb60f4f7592c51b1eb3c3c46f9f",
+    "url": "/marc_mcBride/index.html"
+  }
+];
