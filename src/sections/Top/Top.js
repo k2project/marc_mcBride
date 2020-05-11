f4e82a34a294c8adf2c3fe60d481fc5748d02dbb
k2projct <kopczkrzy@gmail.com>
2020-05-11 10:13:45 +0100

    test update

--- src/sections/Top/Top.js
+++ src/sections/Top/Top.js
@@ -1,42 +1,47 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import './Top.scss';
 import Nav from './../../components/Nav/Nav';
 
-export default function Top (){
-    useEffect(()=>{
-        window.addEventListener('scroll',handleScroll);
-        return ()=>{
-            window.removeEventListener('scroll',handleScroll);
-
-        }
+export default function Top() {
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
     });
-    return(
-        <section className="Top" id="welcome">
-            <div className="cover bg"></div>
-            <div className="cover"></div>
-            <Nav/>
-            <div className="wrapper">
-                <div className="Top__content">
-                    <h1>MARC <br/>McBRIDE</h1>
-                    <h2>Theatre Music Director, Conductor & Pianist</h2>
+    return (
+        <section className='Top' id='welcome'>
+            <div className='cover bg'></div>
+            <div className='cover'></div>
+            <Nav />
+            <div className='wrapper'>
+                <div className='Top__content'>
+                    <h1>
+                        MARC <br />
+                        McBRIDE
+                    </h1>
+                    <h2>Theatre Music Director</h2>
                     {/* <h2>Music Director</h2> */}
-                    <p>Coffee lover. Dog lover. <br/>Health & Fitness junkie. Traveller.</p>
+                    <p>
+                        Coffee lover. Dog lover. <br />
+                        Health & Fitness junkie. Traveller.
+                    </p>
                 </div>
-
             </div>
         </section>
-    )
-};
-function handleScroll(){
-        let topElRectTop = document.querySelector('.Top').getBoundingClientRect().top;
-        const topEl = document.querySelector('.Top__content');
+    );
+}
+function handleScroll() {
+    let topElRectTop = document.querySelector('.Top').getBoundingClientRect()
+        .top;
+    const topEl = document.querySelector('.Top__content');
 
-        if(topElRectTop < 0){
-            const opacity = 1+( topElRectTop/300 );
-            topEl.style.top = topElRectTop * -.5 + 'px';
-            topEl.style.opacity = opacity;
-        }else{
-            topEl.style.top ='0px';
-            topEl.style.opacity = 1;
-        }
+    if (topElRectTop < 0) {
+        const opacity = 1 + topElRectTop / 300;
+        topEl.style.top = topElRectTop * -0.5 + 'px';
+        topEl.style.opacity = opacity;
+    } else {
+        topEl.style.top = '0px';
+        topEl.style.opacity = 1;
+    }
 }
