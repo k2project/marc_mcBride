95a0c63c4980f2bed37d2d6ebeac8c788f076839
k2projct <kopczkrzy@gmail.com>
2020-06-24 08:51:12 +0100

    corporate work

--- src/sections/About/About.js
+++ src/sections/About/About.js
@@ -7,14 +7,18 @@ export default function About() {
             <div className='wrapper' id='main'>
                 <h2 className='section__title'>About</h2>
                 <div className='section__content'>
-                    <p>
-                        Scottish born and raised. Now based predominantly in the
-                        U.S.
-                    </p>
                     <p>Studied at The Royal Academy of Music, London. </p>
                     <p>
                         Previously, a Bachelor of Laws (LL.B) at The University
                         of Dundee, Scotland.
+                    </p>
+                    <p>
+                        Before becoming a full time music director, performing
+                        credits included; Wicked The Musical (Apollo Victoria,
+                        West End), BBC Proms 2016/17/18 (Royal Albert Hall),
+                        various performances with The John Wilson Orchestra and
+                        Les Misérables 20th Anniversary (Queens Theatre, West
+                        End).
                     </p>
                     <p>Avid language learner. </p>
                 </div>
--- src/sections/Work/Work.js
+++ src/sections/Work/Work.js
@@ -2,7 +2,41 @@ import React from 'react';
 import './Work.scss';
 import cv from '../../media/Marc_McBride_Online_CV.pdf';
 
+import attitude from '../../media/logos/attitude.png';
+import bb from '../../media/logos/bb.png';
+import carnival from '../../media/logos/carnival.png';
+import casino from '../../media/logos/casino2.png';
+import davines from '../../media/logos/davines.png';
+import fairmont from '../../media/logos/fairmont.png';
+import gq from '../../media/logos/gq.png';
+import harpa2 from '../../media/logos/harpa2.png';
+import iaea from '../../media/logos/ia.png';
+import rcc from '../../media/logos/rcc2.png';
+import seaborn from '../../media/logos/seaborn.png';
+import savoyhotel from '../../media/logos/savoy2.png';
+
+// TCB Group London,  ,
 export default function Work() {
+    const logos = [
+        { name: 'Attitude Magazine ', src: attitude },
+        { name: 'Celebrity Cruises International', src: rcc },
+
+        { name: 'British GQ Magazine', src: gq },
+        { name: 'Fairmont Hotel Group', src: fairmont },
+        { name: 'Beyond Brodway', src: bb },
+        { name: 'Seabourn', src: seaborn },
+        { name: 'Davines International', src: davines },
+        { name: 'Carnival Corporation & PLC', src: carnival },
+        { name: 'The Savoy Hotel London', src: savoyhotel },
+        { name: 'The Hippodrome Casino London', src: casino },
+        { name: 'The International Atomic Energy Agency Vienna', src: iaea },
+        { name: 'Harpa', src: harpa2 },
+    ];
+    const logosList = logos.map((logo) => (
+        <li key={logo.name}>
+            <img src={logo.src} alt={logo.name} />
+        </li>
+    ));
     return (
         <section className='Work' id='work'>
             <div className='wrapper'>
@@ -67,39 +101,25 @@ export default function Work() {
                         </ul>
                     </section>
                     <section>
-                        <h3 id='music-performiong'>Performing:</h3>
-                        <ul aria-labelledby='music-performing'>
-                            <li>
-                                Wicked The Musical - Apollo Victoria, West End
-                            </li>
-                            <li>
-                                John Wilson Orchestra - Music of MGM - Royal
-                                Albert Hall / UK Tour
-                            </li>
-                            <li>
-                                BBC Proms 2016/2017/2018 - Royal Albert Hall
-                            </li>
-                            <li>
-                                Music Of Danny Elfman &amp; Tim Burton - Royal
-                                Albert Hall / UK Tour
-                            </li>
-                            <li>
-                                Les Miserables 20th Anniversary - Queens
-                                Theatre, West End
-                            </li>
+                        <h3 id='corporate'>Commercial/Corporte:</h3>
+                        <ul
+                            aria-labelledby='corporate'
+                            className='Work__corporate'
+                        >
+                            {logosList}
                         </ul>
                     </section>
-                    <section className='Work__download'>
-                        <a
-                            href={cv}
-                            download
-                            className='btn'
-                            onMouseDown={(e) => e.preventDefault()}
-                        >
-                            Download PDF CV
-                        </a>
-                    </section>
                 </div>
+                <section className='Work__download'>
+                    <a
+                        href={cv}
+                        download
+                        className='btn'
+                        onMouseDown={(e) => e.preventDefault()}
+                    >
+                        Download PDF CV
+                    </a>
+                </section>
             </div>
         </section>
     );
